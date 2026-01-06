8143ee4242f17aa50c4db5d99cbd98794a70df8f
LeonardoSantosBR <leosantosmelo07@gmail.com>
2026-01-06 14:29:39 -0300

    fix: ajustes calculadora

--- Main.java
+++ Main.java
@@ -1,10 +1,44 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args){
-        App calculadora = new App();
-        System.out.println("valor inicial: " + calculadora.value);
-        System.out.println("somar 100: " + calculadora.add(100));
-        System.out.println("multiplicar por 5: " + calculadora.multiply(5));
-        System.out.println("dividir por 2: " + calculadora.divide(2));
-        System.out.println("resetar: " + calculadora.reset());
+        App calculator = new App();
+        Scanner scanner = new Scanner(System.in);
+        boolean in_operation = true;
+
+        while (in_operation) {
+            System.out.println("Digite um número:");
+            int number = scanner.nextInt();
+
+           System.out.print("Digite a operação (+, -, *, /, C): ");
+           char operation = scanner.next().charAt(0);
+
+           switch (operation) {
+            case '+':
+                calculator.add(number);
+                break;
+            case '-':
+                calculator.subtract(number);
+                break;
+            case '*':
+                calculator.multiply(number);
+                break;
+            case '/':
+                calculator.divide(number);
+                break;
+            case 'C':
+                calculator.reset();
+                break;
+            default:
+                break;
+           }
+
+            System.out.print("Deseja finalizar a operação? (S, N)");
+            char is_finished = scanner.next().charAt(0);
+
+            if(is_finished == 'S') in_operation= false;
+        };
+        System.out.println("Resultado final: " + calculator.value);
+        scanner.close();
     }
 }
